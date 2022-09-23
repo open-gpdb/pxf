b099e6099dd47dd4477036ede98fb96bd1aabae4
Himanshu Pandey <pandeyhi@vmware.com>
2022-09-23 15:03:40 -0700

    Fixing Tinc tests

--- automation/sqlrepo/features/general/alter/pxfwritable_import/without_column_projection/sql/query01.sql
+++ automation/sqlrepo/features/general/alter/pxfwritable_import/without_column_projection/sql/query01.sql
@@ -10,6 +10,9 @@
 -- m/,.*line (.*)/
 -- s/,.*line (.*)//g
 --
+-- m/ALTER/
+-- s/ALTER EXTERNAL TABLE/ALTER FOREIGN TABLE/g
+--
 -- end_matchsubs
 
 -- sets the bytea output to the expected by the tests
