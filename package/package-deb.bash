bb8faa7b28b30b584a24c6007e4447407ff5b399
Nikolay Antonov <ostinru@gmail.com>
2025-04-07 20:43:32 +0500

    fix cloudberry build

--- package/package-deb.bash
+++ package/package-deb.bash
@@ -5,6 +5,17 @@ cat <<EOF
 greenplum-pxf-6 (${PXF_PKG_VERSION}) stable; urgency=low
 
   * open-gpdb/pxf autobuild
+
+-- ${BUILD_USER} <${BUILD_USER}@$(hostname)>  $(date +'%a, %d %b %Y %H:%M:%S %z')
+EOF
+}
+
+function print_cb_changelog() {
+cat <<EOF
+greenplum-pxf-cb (${PXF_PKG_VERSION}) stable; urgency=low
+
+  * open-gpdb/pxf autobuild
+
 -- ${BUILD_USER} <${BUILD_USER}@$(hostname)>  $(date +'%a, %d %b %Y %H:%M:%S %z')
 EOF
 }
